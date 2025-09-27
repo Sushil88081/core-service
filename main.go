76ebd1c3fb5cbf22631e05d4ee530c655f355bc2
sushil88081 <sushil88081@gmail.com>
2025-09-28 02:19:19 +0530

    fix

--- main.go
+++ main.go
@@ -1,18 +1,1 @@
 package main
-
-import "fmt"
-
-type User struct {
-	Name string
-	Age  int
-}
-
-func main() {
-
-	u := User{
-		Name: "sushil",
-		Age:  20,
-	}
-	fmt.Println("main func", u.Name)
-
-}
--- repo/crudMethod.go
+++ repo/crudMethod.go
@@ -1,9 +1,47 @@
 package repo
 
+import "gorm.io/gorm"
+
 type CrudEntity[T any] interface {
 	Create(entity T) error
 	GetById(id string) (*T, error)
 	Delete(id string) error
-	Update(entity T) error
+	Update(entity T) (*T, error)
 	GetAll() ([]T, error)
+}
+
+type BaseModel[T any] struct {
+	Db *gorm.DB
+}
+
+func (b *BaseModel[T]) Create(entity T) error {
+	return b.Db.Create(&entity).Error
+}
+
+func (b *BaseModel[T]) GetById(id string) (*T, error) {
+	var entity T
+	if err := b.Db.First(&entity, "id = ?", id).Error; err != nil {
+		return nil, err
+	}
+	return &entity, nil
+}
+
+func (b *BaseModel[T]) Delete(id string) error {
+	var entity T
+	return b.Db.Delete(&entity, "id = ?", id).Error
+}
+
+func (b *BaseModel[T]) Update(entity T) (*T, error) {
+	if err := b.Db.Save(&entity).Error; err != nil {
+		return nil, err
+	}
+	return &entity, nil
+}
+
+func (b *BaseModel[T]) GetAll() ([]T, error) {
+	var entities []T
+	if err := b.Db.Find(&entities).Error; err != nil {
+		return nil, err
+	}
+	return entities, nil
 }
