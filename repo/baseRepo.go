948825fde1a976ed36ebce4bef084180d9eadd86
sushil88081 <sushil8881@gmail.com>
2025-09-29 03:18:18 +0530

    fix:update method signature

--- repo/baseRepo.go
+++ repo/baseRepo.go
@@ -6,7 +6,7 @@ type CrudEntity[T any] interface {
 	Create(entity T) error
 	GetById(id string) (*T, error)
 	Delete(id string) error
-	Update(entity T) (*T, error)
+	Update(id string, entity *T) (T, error)
 	GetAll() ([]T, error)
 }
 
@@ -31,11 +31,22 @@ func (b *BaseModel[T]) Delete(id string) error {
 	return b.Db.Delete(&entity, "id = ?", id).Error
 }
 
-func (b *BaseModel[T]) Update(entity T) (*T, error) {
-	if err := b.Db.Save(&entity).Error; err != nil {
-		return nil, err
+func (b *BaseModel[T]) Update(id string, entity *T) (T, error) {
+	var existing T
+
+	// Pehle check karo ki record exist karta hai ya nahi
+	if err := b.Db.First(&existing, "id = ?", id).Error; err != nil {
+		var zero T
+		return zero, err
 	}
-	return &entity, nil
+
+	// Ab update karo
+	if err := b.Db.Model(&existing).Updates(entity).Error; err != nil {
+		var zero T
+		return zero, err
+	}
+
+	return *entity, nil
 }
 
 func (b *BaseModel[T]) GetAll() ([]T, error) {
