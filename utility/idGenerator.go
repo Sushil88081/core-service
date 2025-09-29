e11ed8ea2a16bb5e7988708054267c98de03190d
sushil88081 <sushil8881@gmail.com>
2025-09-29 16:24:50 +0530

    fix:id

--- utility/idGenerator.go
+++ utility/idGenerator.go
@@ -2,13 +2,15 @@ package utility
 
 import (
 	"math/rand"
+	"strconv"
 	"time"
 )
 
-func GenerateId() int64 {
+func GenerateId() string {
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	min := int64(1)
 	max := int64(999999999999)
 	id := r.Int63n(max-min+1) + min
-	return id
+	idStr := strconv.FormatInt(id, 10)
+	return idStr
 }
